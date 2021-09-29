f977337edf63b19494815e486c8238e6a7703588
Carter Temm <cartertemm@gmail.com>
2021-09-29 13:12:20 -0700

    bump version to 0.4 for upload

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
 	name="PyBeamIt",
-	version="0.3",
+	version="0.4",
 	packages=find_packages(),
 	install_requires=open("requirements.txt", "r").read().split("\n"),
 	author="Carter Temm",
